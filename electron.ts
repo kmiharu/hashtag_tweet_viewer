6c51ea87a9dfce3638c4cb7fdd53f2c7d1eda43a
hirony-jp <jp.hiroki48@gmail.com>
2019-06-25 21:28:29 +0900

    electron

---
+++ electron.ts
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+const appLoad = () => {
+  win.loadURL('http://localhost:3000');
+};
+
+const createWindow = () => {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  appLoad();
+
+  win.on('closed', () => {
+    win = null;
+  });
+};
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
