d6014ef6d0b9e59a9e707b2026fda401477ad7f5
hirony-jp <jp.hiroki48@gmail.com>
2019-06-25 22:58:45 +0900

    font awesome test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const App: React.FC = () => {
   return (
     <div>
       <p>Hello HTV</p>
+      <FontAwesomeIcon icon={['fab', 'twitter']} />
     </div>
   );
 };
