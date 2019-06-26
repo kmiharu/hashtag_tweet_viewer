cc794f7ff150c10c0aff541d84116811116e1255
hirony-jp <jp.hiroki48@gmail.com>
2019-06-27 01:15:49 +0900

    create tweet view

--- src/components/TweetView.tsx
+++ src/components/TweetView.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { CardContent, Typography } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CustomCard from './style/CustomCard';
 
 const TweetView: React.FC = () => {
   return (
     <>
-      <p>TweetView</p>
+      <CustomCard>
+        <CardContent>
+          <Typography variant="h3">
+            <FontAwesomeIcon icon={['fab', 'twitter']} /> &#35;&nbsp;hirony_live
+          </Typography>
+          <Typography variant="h4">@_hirony</Typography>
+          <Typography variant="h5">Hello World #hirony_live</Typography>
+        </CardContent>
+      </CustomCard>
     </>
   );
 };
---
+++ src/components/style/CustomCard.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { Card } from '@material-ui/core';
+
+const CustomCard = styled(Card)`
+  && {
+    bottom: 8px;
+    left: 8px;
+    position: absolute;
+    right: 8px;
+  }
+`;
+
+export default CustomCard;
