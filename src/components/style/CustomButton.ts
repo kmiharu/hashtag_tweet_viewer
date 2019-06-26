d96b463e793b3a4a0df6e1cfa2d94fa4da125414
hirony-jp <jp.hiroki48@gmail.com>
2019-06-27 00:46:53 +0900

    remove dust

--- src/components/style/CustomButton.ts
+++ src/components/style/CustomButton.ts
@@ -1,10 +1,6 @@
 import styled from 'styled-components';
 import { Button } from '@material-ui/core';
 
-interface ButtonProps {
-  isPrimary: boolean;
-}
-
 const CustomButton = styled(Button)`
   && {
     padding-left: 40px;
