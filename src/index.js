de6d5b21850f2b1bcf6f224cdccef83f1753f00c
hirony-jp <jp.hiroki48@gmail.com>
2020-04-17 19:27:19 +0900

    install fontawesome

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import { library } from '@fortawesome/fontawesome-svg-core'; //fontawesomeのコアファイル
+// import { fab } from '@fortawesome/free-brands-svg-icons'; //fontawesomeのbrandアイコンのインポート
+// import { fas } from '@fortawesome/free-solid-svg-icons'; //fontawesomeのsolidアイコンのインポート
+// import { far } from '@fortawesome/free-regular-svg-icons'; //fontawesomeのregularアイコンのインポート
 import * as serviceWorker from './serviceWorker';
 
+// library.add(fab, fas, far)
 ReactDOM.render(
   <React.StrictMode>
     <App />
